18daabf62fc7291326f521a724faaf45f083b621
Monjur Morshed <monjurmorshed794@outlook.com>
2017-07-11 22:01:44 +0600

    Spring security dependency added.

--- src/main/java/com/gradbook/gradbookfriend/userfriend/helper/UserFriendResourceHelper.java
+++ src/main/java/com/gradbook/gradbookfriend/userfriend/helper/UserFriendResourceHelper.java
@@ -1,7 +1,19 @@
 package com.gradbook.gradbookfriend.userfriend.helper;
 
+import com.gradbook.gradbookfriend.user.User;
+import com.gradbook.gradbookfriend.userfriend.UserFriend;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
 /**
  * Created by Munna on 10-Jul-17.
  */
+@Component
 public class UserFriendResourceHelper {
+
+    List<User> getFriends(){
+
+        return null;
+    }
 }
